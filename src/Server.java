976694010b875d21450159d909ca5adf0159c0d3
arMin <armin.gharachorloo@gmail.com>
2025-05-26 00:02:08 +0330

    شروع فاز 2

---
+++ src/Server.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+import com.google.gson.Gson;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+
+                new Thread(() -> {
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                        String message = in.readLine();
+
+                        Gson gson = new Gson();
+                        Request request = gson.fromJson(message, Request.class);
+
+                        String response= "done" ;
+
+
+                        out.write(response);
+                        out.newLine();
+                        out.flush();
+
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
