037c4cffd118623c5864d9b26d50fa9c5e5fbd11
arMin <armin.gharachorloo@gmail.com>
2025-05-31 10:41:52 +0330

    سوییچ کیس های بک اند

--- src/Database.java
+++ src/Database.java
@@ -1,18 +1,90 @@
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Database {
-    List<Song> songs;
-    List<User> users;
+    private List<Song> songs;
+    private List<User> users;
+    private final Gson gson;
 
-     Gson gson;
+    private static final String USERS_FILE = "users.json";
+    private static final String SONGS_FILE = "songs.json";
 
-
-    void removeUser(User user){
-        users.remove(user);
+    public Database() {
+        gson = new Gson();
+        songs = new ArrayList<>();
+        users = new ArrayList<>();
+        loadUsers();
+        loadSongs();
     }
-    void addUser(User user){
+
+    public void addUser(User user) {
         users.add(user);
+        saveUsers();
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+        saveUsers();
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+        saveSongs();
+    }
+
+    public void removeSong(Song song) {
+        songs.remove(song);
+        saveSongs();
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void saveUsers() {
+        try (FileWriter writer = new FileWriter(USERS_FILE)) {
+            gson.toJson(users, writer);
+        } catch (IOException e) {
+            System.out.println("Failed to save users: " + e.getMessage());
+        }
+    }
+
+    public void saveSongs() {
+        try (FileWriter writer = new FileWriter(SONGS_FILE)) {
+            gson.toJson(songs, writer);
+        } catch (IOException e) {
+            System.out.println("Failed to save songs: " + e.getMessage());
+        }
+    }
+
+    public void loadUsers() {
+        try (FileReader reader = new FileReader(USERS_FILE)) {
+            Type userListType = new TypeToken<List<User>>() {}.getType();
+            users = gson.fromJson(reader, userListType);
+            if (users == null) users = new ArrayList<>();
+        } catch (IOException e) {
+            users = new ArrayList<>();
+        }
+    }
+
+    public void loadSongs() {
+        try (FileReader reader = new FileReader(SONGS_FILE)) {
+            Type songListType = new TypeToken<List<Song>>() {}.getType();
+            songs = gson.fromJson(reader, songListType);
+            if (songs == null) songs = new ArrayList<>();
+        } catch (IOException e) {
+            songs = new ArrayList<>();
+        }
     }
 }
